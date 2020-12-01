d49bff64775619ed4eb87410f190b858a4ab6909
natezamiatin <4406015@gmail.com>
2020-11-30 16:19:58 -0800

    basick for new Portfolio full page

---
+++ app.js
@@ -0,0 +1,26 @@
+new fullpage('#fullpage', {
+    autoScrolling:true,
+    navigation: true,
+    licenseKey:null,
+    anchors: ['section1','section2','section3','section4',],
+    navigationTooltips: ['Intro','What I do', 'My story','Get in touch'],
+    showActiveTooltips : true,
+    scrollingSpeed: 1500,
+
+
+   onLeave:(origin, destination,direction)=>{
+       const section =destination.item
+       const title = section.querySelectorAll('h1')
+
+    //    const tl = new TimelineMax({ delay : 0.5})
+    //    console.log(tl.formTo())
+    //    tl.fromTo(title, 1.5, {y : "50", opacity : 0},{y : 0, opacity : 1} )
+    if (destination.index === 1){
+        const title = section.querySelectorAll('h1')
+
+
+    }
+      
+   }
+})
+
